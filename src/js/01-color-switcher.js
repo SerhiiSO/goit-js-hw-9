fb99ae3845f4b0934fb721f5d9691954bb414e79
Serhii Semenov <84242725+SerhiiSO@users.noreply.github.com>
2021-11-09 18:44:15 +0200

    color-sw-added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const colorType = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyEl: document.querySelector('body'),
+};
+let bodyColorTimer = null;
+colorType.stopBtn.setAttribute('disabled', true);
+
+colorType.startBtn.addEventListener('click', onStartBtnClick);
+colorType.stopBtn.addEventListener('click', onStopBtnClick);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+};
+
+function onStartBtnClick() {
+    bodyColorTimer = setInterval(() => colorType.bodyEl.style.backgroundColor = getRandomHexColor(), 1000);
+    colorType.startBtn.setAttribute('disabled', true);
+    colorType.stopBtn.removeAttribute('disabled');
+};
+
+function onStopBtnClick() {
+    clearInterval(bodyColorTimer);
+    colorType.startBtn.removeAttribute('disabled');
+    colorType.stopBtn.setAttribute('disabled', true);
+};
+
+
 
